0fe26917acd2e07a97432b331fb74e110fae5863
BryanQuesada <BryanQuesada@CR-BQUESADA2>
2023-09-28 15:17:47 -0600

    Filter was added

--- AccountsPayable/App_Start/RouteConfig.cs
+++ AccountsPayable/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AccountsPayable
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Access", action = "LogIn", id = UrlParameter.Optional }
             );
         }
     }
---
+++ AccountsPayable/Filters/VerifySession.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AccountsPayable.Controllers;
+using AccountsPayable.Models;
+
+
+
+namespace AccountsPayable.Filters
+{
+    public class VerifySession : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            var ouser = (TB_LOG_IN)HttpContext.Current.Session["User"];
+            //Avoid redirection if the user is not signed in
+            if (ouser == null)
+            {
+
+                if (filterContext.Controller is MainController == true)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Access/LogIn");
+                }
+
+                
+
+            }
+            else
+            {
+                // if user is signed in avoid redirection to login controller
+                if (filterContext.Controller is AccessController == true)
+                {
+
+                    filterContext.HttpContext.Response.Redirect("~/Main/Index");
+                }
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+
+
+
+}
