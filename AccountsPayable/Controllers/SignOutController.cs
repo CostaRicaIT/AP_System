9f6167b4771d7a039b29401a9949c1051fa01cb6
BryanQuesada <BryanQuesada@CR-BQUESADA2>
2023-09-29 16:03:09 -0600

    LogOut Implementation

---
+++ AccountsPayable/Controllers/SignOutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountsPayable.Controllers
+{
+    public class SignOutController : Controller
+    {
+        // GET: LogOut
+        public ActionResult EndSession()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Login", "Access");
+        }
+    }
+}
