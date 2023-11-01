fbd4d4bb6038a600aabff0d88a334063a6b22b30
fernando-zamora <fernando.zamora@ayahealthcare.com>
2023-11-01 16:58:49 -0600

    initial implementation of dashboard

---
+++ AccountsPayable/Scripts/dashboard.js
@@ -0,0 +1,71 @@
+﻿var table; // Declare the 'table' variable in a global scope
+
+function initializeDataTable() {
+    table = $('#dataTable').DataTable({
+        autoWidth: true,
+        stateSave: true,
+        responsive: true,
+        dom: 'lBfrtip',
+        buttons: [
+            {
+                text: '<i class="fas fa-filter"></i>',
+                className: 'filterButton',
+                action: function (e, dt, node, config) {
+                    $('#filtersModal').modal('show');
+                }
+            }
+        ]
+    });
+
+    $('a.toggle-vis').on('click', function (e) {
+        e.preventDefault();
+
+        var column = table.column($(this).attr('data-column'));
+        column.visible(!column.visible());
+
+
+    });
+    $('#showColumnsButton').click(function () {
+        $('#filtersModal').modal('hide');
+        showFilterModal();
+    });
+    $('#resetColumnsButton').click(function (){
+        resetFilters();
+    });
+}
+function showFilterModal() {
+    // Open the modal
+    $('#toggleColumnsModal').modal('show');
+
+    // Initialize the checkboxes in the modal based on current visibility
+    $('.toggle-column').each(function () {
+        var columnIdx = $(this).data('column');
+        var isVisible = table.column(columnIdx).visible();
+        $(this).prop('checked', isVisible);
+    });
+
+    // Save button click event
+    $('#saveColumnVisibility').on('click', function () {
+        // Iterate through checkboxes and update column visibility
+        $('.toggle-column').each(function () {
+            var columnIdx = $(this).data('column');
+            var isVisible = $(this).is(':checked');
+            table.column(columnIdx).visible(isVisible);
+        });
+
+        // Close the modal
+        $('#toggleColumnsModal').modal('hide');
+    });
+}
+
+function resetFilters() {
+    table.columns().visible(true);
+    table.state.clear();
+    $('#filtersModal').modal('hide');
+
+}
+
+// Call the function to initialize the DataTable when the document is ready
+$(document).ready(function () {
+    initializeDataTable();
+});
