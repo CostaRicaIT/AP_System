5ab268e7287ce5fcb55a7c0dfd477c06cc04bef2
BryanQuesada <BryanQuesada@CR-BQUESADA2>
2023-12-07 11:18:19 -0600

    Connection String changed to prod

--- AccountsPayable/Models/TB_HIGHLIGHTS.cs
+++ AccountsPayable/Models/TB_HIGHLIGHTS.cs
@@ -18,8 +18,6 @@ namespace AccountsPayable.Models
         public TB_HIGHLIGHTS()
         {
             this.TB_TEMPLATE1 = new HashSet<TB_TEMPLATE>();
-            this.TB_TEMPLATE = new HashSet<TB_TEMPLATE>();
-            this.TB_TEMPLATE1 = new HashSet<TB_TEMPLATE>();
         }
     
         public int HIGHLIGHTS_ID { get; set; }
@@ -36,8 +34,6 @@ namespace AccountsPayable.Models
         public virtual TB_TEMPLATE TB_TEMPLATE { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<TB_TEMPLATE> TB_TEMPLATE1 { get; set; }
-        public virtual ICollection<TB_TEMPLATE> TB_TEMPLATE { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<TB_TEMPLATE> TB_TEMPLATE1 { get; set; }
+  
     }
 }
