d3bf41985438ca66d04d6a5abc92e3726db11eef
fernando-zamora <fernando.zamora@ayahealthcare.com>
2024-03-19 10:47:08 -0600

    changes to apply TinyMCE readonly fields

---
+++ AccountsPayable/Scripts/TinyEditorConfig.js
@@ -0,0 +1,17 @@
+﻿tinymce.init({
+    selector: 'textarea.TinyEditorReadOnly', //select field using textarea.class
+    promotion: false, //Hide upgrade promotion of TinyMCE
+    branding: false, //Hide TinyMCE logo
+    readonly: true,
+    statusbar: false,
+    menubar: false,
+    toolbar: "undo redo |forecolor |backcolor "
+});
+tinymce.init({
+    selector: 'textarea.TinyEditor', //select field using textarea.class
+    promotion: false, //Hide upgrade promotion of TinyMCE
+    branding: false, //Hide TinyMCE logo
+    statusbar: false,
+    menubar: false,
+    toolbar: "undo redo |forecolor |backcolor "
+});
