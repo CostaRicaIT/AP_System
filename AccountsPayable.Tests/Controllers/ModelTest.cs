1650a366d4e8038143b29629883e93d5c462bc38
Fernando Zamora <fernando.zamora@ayahealthcare.com>
2024-08-02 10:02:48 -0600

    Added Test to check if AllowHTML property is added on the required fields

---
+++ AccountsPayable.Tests/Controllers/ModelTest.cs
@@ -0,0 +1,40 @@
+﻿using AccountsPayable.Models;
+using NUnit.Framework;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace AccountsPayable.Tests.Controllers
+{
+    public class ModelTest
+    {
+        [Test]
+        public void AllowHTML_Attribute_TB_TEMPLATE()
+        {
+            // Arrange
+            var modelType = typeof(TB_TEMPLATE);
+            var propertiesWithTinyMCE = new[]
+            {
+            "TEMP_ORACLE_DESCRIPTION",
+            "TEMP_ORACLE_NOTES",
+            "TEMP_ORACLE_INSTRUCTIONS",
+            "TEMP_BILLING_PERIOD",
+            "TEMP_VSU",
+            "TEMP_W9_W8",
+            "TEMP_INVOICE_NOTES",
+            "",
+            "",
+        };
+
+            // Act & Assert
+            foreach (var propertyName in propertiesWithTinyMCE)
+            {
+                var propertyInfo = modelType.GetProperty(propertyName);
+                Assert.IsNotNull(propertyInfo, $"{propertyName} property not found on {modelType.Name} model.");
+
+                var hasAllowHtmlAttribute = propertyInfo.GetCustomAttributes(typeof(AllowHtmlAttribute), false).Any();
+                Assert.IsTrue(hasAllowHtmlAttribute, $"{propertyName} property on {modelType.Name} model does not have the [AllowHtml] attribute.");
+            }
+        }
+    }
+}
