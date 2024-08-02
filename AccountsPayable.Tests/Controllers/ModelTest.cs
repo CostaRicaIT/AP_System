71b48f82221946ed9192bd93a677ce41ff702be3
Fernando Zamora <fernando.zamora@ayahealthcare.com>
2024-08-02 10:04:01 -0600

    Updated test to check Allow HTML on all the required tables

--- AccountsPayable.Tests/Controllers/ModelTest.cs
+++ AccountsPayable.Tests/Controllers/ModelTest.cs
@@ -22,8 +22,54 @@ namespace AccountsPayable.Tests.Controllers
             "TEMP_VSU",
             "TEMP_W9_W8",
             "TEMP_INVOICE_NOTES",
-            "",
-            "",
+        };
+
+            // Act & Assert
+            foreach (var propertyName in propertiesWithTinyMCE)
+            {
+                var propertyInfo = modelType.GetProperty(propertyName);
+                Assert.IsNotNull(propertyInfo, $"{propertyName} property not found on {modelType.Name} model.");
+
+                var hasAllowHtmlAttribute = propertyInfo.GetCustomAttributes(typeof(AllowHtmlAttribute), false).Any();
+                Assert.IsTrue(hasAllowHtmlAttribute, $"{propertyName} property on {modelType.Name} model does not have the [AllowHtml] attribute.");
+            }
+        }
+
+        [Test]
+        public void AllowHTML_Attribute_TB_HIGHLIGHTS()
+        {
+            // Arrange
+            var modelType = typeof(TB_HIGHLIGHTS);
+            var propertiesWithTinyMCE = new[]
+            {
+            "HIGHLIGHTS",
+            "HIGHLIGHTS_COMMENTS",
+            "HIGHLIGHTS_INSTRUCTIONS",
+            "HIGHLIGHTS_EXCEPTIONS",
+            "HIGHLIGHTS_COMMON_ISSUES",
+            "HIGHLIGHTS_SUPPLIER_AGENCY",
+            "HIGHLIGHTS_TEMPLATE_COMMENTS",
+        };
+
+            // Act & Assert
+            foreach (var propertyName in propertiesWithTinyMCE)
+            {
+                var propertyInfo = modelType.GetProperty(propertyName);
+                Assert.IsNotNull(propertyInfo, $"{propertyName} property not found on {modelType.Name} model.");
+
+                var hasAllowHtmlAttribute = propertyInfo.GetCustomAttributes(typeof(AllowHtmlAttribute), false).Any();
+                Assert.IsTrue(hasAllowHtmlAttribute, $"{propertyName} property on {modelType.Name} model does not have the [AllowHtml] attribute.");
+            }
+        }
+
+        [Test]
+        public void AllowHTML_Attribute_TB_HISTORIC_REMIT()
+        {
+            // Arrange
+            var modelType = typeof(TB_HISTORIC_REMIT);
+            var propertiesWithTinyMCE = new[]
+            {
+            "HISTORIC_REMIT_INFO",            
         };
 
             // Act & Assert
