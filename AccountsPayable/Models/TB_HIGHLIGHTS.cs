b35d499f7d0b455bf8b479ef5d43016d1ac46e2e
Fernando Zamora <fernando.zamora@ayahealthcare.com>
2024-10-25 15:03:15 -0600

    Added AllowHtml property

--- AccountsPayable/Models/TB_HIGHLIGHTS.cs
+++ AccountsPayable/Models/TB_HIGHLIGHTS.cs
@@ -11,6 +11,7 @@ namespace AccountsPayable.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.Web.Mvc;
     
     public partial class TB_HIGHLIGHTS
     {
@@ -24,12 +25,19 @@ namespace AccountsPayable.Models
         public int HIGHLIGHTS_ID { get; set; }
         public Nullable<int> FK_TB_TEMPLATE_ID { get; set; }
         public System.DateTime HIGHLIGHTS_DATE { get; set; }
+        [AllowHtml]
         public string HIGHLIGHTS { get; set; }
+        [AllowHtml]
         public string HIGHLIGHTS_COMMENTS { get; set; }
+        [AllowHtml]
         public string HIGHLIGHTS_INSTRUCTIONS { get; set; }
+        [AllowHtml]
         public string HIGHLIGHTS_EXCEPTIONS { get; set; }
+        [AllowHtml]
         public string HIGHLIGHTS_COMMON_ISSUES { get; set; }
+        [AllowHtml]
         public string HIGHLIGHTS_SUPPLIER_AGENCY { get; set; }
+        [AllowHtml]
         public string HIGHLIGHTS_TEMPLATE_COMMENTS { get; set; }
     
         public virtual TB_TEMPLATE TB_TEMPLATE { get; set; }
