62501fe8cde83c5e749e4b81eab27a7ad113ad9f
Fernando Zamora <fernando.zamora@ayahealthcare.com>
2025-01-31 12:24:58 -0600

    Commented buttons related to Workspace

--- AccountsPayable/Scripts/dashboard.js
+++ AccountsPayable/Scripts/dashboard.js
@@ -123,20 +123,32 @@ function DrawTable() {
                             </button>
                         `;
                     } else if (userRoleId === 2) {
+                        //buttons += `
+                        // <button class="dt-button filterButton edit-button" data-id="${row.Id}" aria-label="Edit">
+                        //        <i class="fas fa-edit"></i>
+                        //    </button>
+                        //    <button class="dt-button filterButton view-button" data-id="${row.Id}" aria-label="View">
+                        //        <i class="fa-solid fa-eye"></i>
+                        //    </button>
+                        //    <button class="dt-button filterButton Workspace-button" data-id="${row.Id}" aria-label="create">
+                        //      <i class="fa-regular fa-file-lines"></i>
+                        //    </button>
+                        //    <button class="dt-button filterButton erase-button" data-id="${row.Id}" aria-label="Delete">
+                        //        <i class="fa-solid fa-eraser"></i>
+                        //    </button>
+                        //`;
                         buttons += `
                          <button class="dt-button filterButton edit-button" data-id="${row.Id}" aria-label="Edit">
                                 <i class="fas fa-edit"></i>
                             </button>
                             <button class="dt-button filterButton view-button" data-id="${row.Id}" aria-label="View">
                                 <i class="fa-solid fa-eye"></i>
-                            </button>   
-                            <button class="dt-button filterButton Workspace-button" data-id="${row.Id}" aria-label="create">
-                              <i class="fa-regular fa-file-lines"></i>
                             </button>
                             <button class="dt-button filterButton erase-button" data-id="${row.Id}" aria-label="Delete">
                                 <i class="fa-solid fa-eraser"></i>
                             </button>
                         `;
+
                     }
 
                     return `<div class="button-container">${buttons}</div>`;
