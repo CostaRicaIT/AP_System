b0f05380137edfc29b273400734aeffa21b40fd3
Fernando Zamora <fernando.zamora@ayahealthcare.com>
2025-02-21 11:13:56 -0600

    Added TinyMCE to new fields

--- AccountsPayable/Scripts/TinyEditorConfig.js
+++ AccountsPayable/Scripts/TinyEditorConfig.js
@@ -7,24 +7,10 @@
     menubar: false,
     toolbar: false,
     license_key: 'gpl'
-    
+
 });
 tinymce.init({
-    selector: '#form-TaxID, #form-Folder, #form-RemitToAccount, #form-DistroCombination, #form-DistroSet', // Targets multiple form IDs
-    promotion: false, // Hide upgrade promotion of TinyMCE
-    branding: false, // Hide TinyMCE logo
-    statusbar: false,
-    menubar: false,
-    toolbar: "undo redo |forecolor |backcolor",
-    license_key: 'gpl',
-    plugins: 'autoresize',
-    autoresize_bottom_margin: 0,
-    autoresize_max_height: 10, // Adjust to fit a single line
-    content_style: "white-space: nowrap; overflow: hidden; margin-left: 1000px; margin-right: 0; position: relative;", // Add margin-left and position to move entire editor
-    width: '70%'  // Adjust width to 100% of its container or specify a pixel value (e.g., 500px)
-});
-tinymce.init({
-    selector: 'textarea.TinyEditor:not(#form-TaxID, #form-Folder, #form-RemitToAccount, #form-DistroCombination, #form-DistroSet)', // Exclude specific forms
+    selector: 'textarea.TinyEditor', //select field using textarea.class
     promotion: false, //Hide upgrade promotion of TinyMCE
     branding: false, //Hide TinyMCE logo
     statusbar: false,
@@ -32,3 +18,33 @@ tinymce.init({
     toolbar: "undo redo |forecolor |backcolor ",
     license_key: 'gpl'
 });
+
+tinymce.init({
+    selector: 'input.TinyEditorSmall, textarea.TinyEditorSmall', //select field using input.class
+    promotion: false, //Hide upgrade promotion of TinyMCE
+    branding: false, //Hide TinyMCE logo
+    statusbar: false,
+    menubar: false,
+    toolbar: "undo redo |forecolor |backcolor ",
+    license_key: 'gpl',
+    plugins: 'autoresize',
+    autoresize_bottom_margin: 0,
+    autoresize_max_height: 10, // Adjust to fit a single line
+    content_style: "white-space: nowrap; overflow: hidden; margin-left: 1000px; margin-right: 0; position: relative;", // Add margin-left and position to move entire editor
+    width: '70%'  // Adjust width to 100% of its container or specify a pixel value (e.g., 500px)
+})
+tinymce.init({
+    selector: 'input.TinyEditorSmallReadOnly, textarea.TinyEditorSmallReadOnly', //select field using input.class
+    promotion: false, //Hide upgrade promotion of TinyMCE
+    branding: false, //Hide TinyMCE logo
+    readonly: true,
+    statusbar: false,
+    menubar: false,
+    toolbar: false,
+    license_key: 'gpl',
+    plugins: 'autoresize',
+    autoresize_bottom_margin: 0,
+    autoresize_max_height: 10, // Adjust to fit a single line
+    content_style: "white-space: nowrap; overflow: hidden; margin-left: 1000px; margin-right: 0; position: relative;", // Add margin-left and position to move entire editor
+    width: '70%'  // Adjust width to 100% of its container or specify a pixel value (e.g., 500px)
+})
--- AccountsPayable/Scripts/WSCrud.js
+++ AccountsPayable/Scripts/WSCrud.js
@@ -205,8 +205,8 @@ function create() {
     // get data from template
     var templateData = {
         TEMP_ID: $("#templateId").text(),
-        WS_WS_TEMP_TAX_ID: $("#Item2_TEMP_TAX_ID").val(),
-        WS_TEMP_FOLDER: $("#Item2_TEMP_FOLDER").val(),
+        WS_WS_TEMP_TAX_ID: tinymce.get("Item2_TEMP_TAX_ID").getContent(),
+        WS_TEMP_FOLDER: tinymce.get("Item2_TEMP_FOLDER").getContent(),
         WS_TEMP_SUPPLIER_NAME: $("#Item2_TEMP_SUPPLIER_NAME").val(),
         WS_TEMP_SUPPLIER_NUMBER: $("#Item2_TEMP_SUPPLIER_NUMBER").val(),
         WS_TEMP_REMIT_TO: $("#Item2_TEMP_REMIT_TO").val(),
@@ -219,13 +219,13 @@ function create() {
         WS_TEMP_W9_W8: tinymce.get("Item2_TEMP_W9_W8").getContent(),
         WS_TEMP_VSU: tinymce.get("Item2_TEMP_VSU").getContent(),
         WS_TEMP_PAYMENT_METHOD: $("#Item2_TEMP_PAYMENT_METHOD").val(),
-        WS_TEMP_REMIT_TOACCOUNT: $("#Item2_TEMP_REMIT_TOACCOUNT").val(),
+        WS_TEMP_REMIT_TOACCOUNT: tinymce.get("Item2_TEMP_REMIT_TOACCOUNT").getContent(),
         WS_FK_TB_ORACLE_PAY_TERMS_ID: $("#FK_TB_ORACLE_PAY_TERMS_ID").val(),
         WS_TEMP_INVOICE_DESCRIPTION: $("#Item2_TEMP_INVOICE_DESCRIPTION").val(),
         WS_TEMP_BILLING_PERIOD: tinymce.get("Item2_TEMP_BILLING_PERIOD").getContent(),
         WS_TEMP_BILLING_PERIOD_DATE: $("#Item2_TEMP_BILLING_PERIOD_DATE").val(),
-        WS_TEMP_DISTRIBUTION_SET: $("#Item2_TEMP_DISTRIBUTION_SET").val(),
-        WS_TEMP_DISTRIBUTION_COMBINATION: $("#Item2_TEMP_DISTRIBUTION_COMBINATION").val(),
+        WS_TEMP_DISTRIBUTION_SET: tinymce.get("Item2_TEMP_DISTRIBUTION_SET").getContent(),
+        WS_TEMP_DISTRIBUTION_COMBINATION: tinymce.get("Item2_TEMP_DISTRIBUTION_COMBINATION").getContent(),
         WS_TEMP_ACCOUNTING_DATE: $("#Item2_TEMP_ACCOUNTING_DATE").val(),
         WS_FK_TB_LEGAL_ENTITY_ID: $("#legalEntityDropdown").val(),
         WS_FK_TB_ORGANIZATION_TYPE_ID: $("#organizationTypeDropdown").val(),
@@ -268,7 +268,7 @@ function create() {
         url: '/WSCrud/Create/',
         type: 'POST',
         data: {
-            templateData, workspaceData,WorkspaceCommentsData,WorkspaceLastActionsData,
+            templateData, workspaceData, WorkspaceCommentsData, WorkspaceLastActionsData,
         },
         success: function (data) {
             if (data.success) {
@@ -296,7 +296,7 @@ function update() {
         WS_INVOICE_NUMBER: $("#WS_INVOICE_NUMBER").val(),
         TEMP_ID: $("#templateId").text(),
         WS_TEMP_TAX_ID: $("#WS_TEMP_TAX_ID").val(),
-        WS_TEMP_FOLDER: $("#WS_TEMP_FOLDER").val(),
+        WS_TEMP_FOLDER: tinymce.get("WS_TEMP_FOLDER").getContent(),
         WS_TEMP_SUPPLIER_NAME: $("#WS_TEMP_SUPPLIER_NAME").val(),
         WS_TEMP_SUPPLIER_NUMBER: $("#WS_TEMP_SUPPLIER_NUMBER").val(),
         WS_TEMP_REMIT_TO: $("#WS_TEMP_REMIT_TO").val(),
@@ -309,13 +309,13 @@ function update() {
         WS_TEMP_W9_W8: tinymce.get("WS_TEMP_W9_W8").getContent(),
         WS_TEMP_VSU: tinymce.get("WS_TEMP_VSU").getContent(),
         WS_TEMP_PAYMENT_METHOD: $("#WS_TEMP_PAYMENT_METHOD").val(),
-        WS_TEMP_REMIT_TOACCOUNT: $("#WS_TEMP_REMIT_TOACCOUNT").val(),
+        WS_TEMP_REMIT_TOACCOUNT: tinymce.get("WS_TEMP_REMIT_TOACCOUNT").getContent(),
         WS_FK_TB_ORACLE_PAY_TERMS_ID: $("#WS_FK_TB_ORACLE_PAY_TERMS_ID").val(),
         WS_TEMP_INVOICE_DESCRIPTION: $("#WS_TEMP_INVOICE_DESCRIPTION").val(),
         WS_TEMP_BILLING_PERIOD: tinymce.get("WS_TEMP_BILLING_PERIOD").getContent(),
         WS_TEMP_BILLING_PERIOD_DATE: $("#WS_TEMP_BILLING_PERIOD_DATE").val(),
-        WS_TEMP_DISTRIBUTION_SET: $("#WS_TEMP_DISTRIBUTION_SET").val(),
-        WS_TEMP_DISTRIBUTION_COMBINATION: $("#WS_TEMP_DISTRIBUTION_COMBINATION").val(),
+        WS_TEMP_DISTRIBUTION_SET: tinymce.get("WS_TEMP_DISTRIBUTION_SET").getContent(),
+        WS_TEMP_DISTRIBUTION_COMBINATION: tinymce.get("WS_TEMP_DISTRIBUTION_COMBINATION").getContent(),
         WS_TEMP_ACCOUNTING_DATE: $("#WS_TEMP_ACCOUNTING_DATE").val(),
         WS_FK_TB_LEGAL_ENTITY_ID: $("#legalEntityDropdown").val(),
         WS_FK_TB_ORGANIZATION_TYPE_ID: $("#organizationTypeDropdown").val(),
@@ -328,7 +328,7 @@ function update() {
         WS_CONTACTS_PRIOR: $("#form-Prior").val(),
         WS_FK_TB_APPROVER_ID: $("#approverDropdown").val(),
         WS_TEMP_APPROVER_COMMENTS: $("#WS_TEMP_APPROVER_COMMENTS").val(),
-        
+
     };
 
     var WorkspaceCommentsData = {
